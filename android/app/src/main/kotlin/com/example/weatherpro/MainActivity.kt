0e96ebfe407c6bf1985494f15083efc772209af9
jaskirat singh <singhjaskirat984@gmail.com>
2020-07-13 12:28:02 +0530

    adding weather pro

---
+++ android/app/src/main/kotlin/com/example/weatherpro/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.weatherpro
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:weatherpro/weather_forecast/weather_forecast.dart';
+
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: WeatherForecast(),
+  ));
+}
